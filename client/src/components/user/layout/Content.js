e3c44be5740ef4af30c6175b89d92b47f29eb4ee
ahmed haggag <ahmedhaggagrady@gmail.com>
2023-04-30 13:49:33 +0300

    spearate client from server

--- client/src/components/user/layout/Content.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { Box, Container } from "@mui/material"
-export default function Content({ children }) {
-    return <Container marginTop={2}>{children}</Container>;
-}
--- client/src/constants/index.js
+++
@@ -1,2 +0,0 @@
-export const SRC_IMAGE = "SRC_IMAGE"
-export const BUFFER_IMAGE = "BUFFER_IMAGE"
